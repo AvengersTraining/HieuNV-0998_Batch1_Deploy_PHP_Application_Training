88abd1828226217ce165118fcda67b4aea2fcbdd
hieunv-0998 <49262782+hieunv-0998@users.noreply.github.com>
2019-10-22 18:28:55 +0700

    Revert "Add Deployer and config Sun* CI."

--- app/User.php
+++ app/User.php
@@ -16,9 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password',
     ];
 
     /**
@@ -27,8 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
     /**
